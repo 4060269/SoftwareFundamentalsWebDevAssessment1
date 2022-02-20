9d0b0dd737ee7ef5e093c9faaa86c8ffa8c8e32a
4060269 <98860251+4060269@users.noreply.github.com>
2022-02-21 10:58:28 +1100

    new functions.html

---
+++ decisions-2.js
@@ -0,0 +1,16 @@
+switch (Movies){
+    case "Spiderman":
+        text = "Spiderman is good";
+        break;
+
+    case "John Wick":
+        text = "John Wick is lit";
+        break;
+
+    case "Thor":
+        text = "I like Thor's hammer";
+        break;
+
+    default:
+        text = "I've never heard of that movie before...";
+}
